4c376f716dca1e56f62313f3dba7d64e2c723665
aneesabeck <aneesabeck@gmail.com>
2024-07-01 11:06:34 -0700

    fetching hobbies

--- client/src/SetHobby.jsx
+++ client/src/SetHobby.jsx
@@ -4,6 +4,7 @@ import { Link, useParams, Navigate } from 'react-router-dom'
 function SetHobby() {
     const [dbHobbies, setDBHobbies] = useState([])
     const [userInterests, setUserInterests] = useState([])
+    const [apiHobbies, setAPIHobbies] = useState([])
     const { username } = useParams();
 
     const fetchUserInterests = () => {
@@ -21,6 +22,7 @@ function SetHobby() {
             console.error('error fetching hobbies:', error)
           })
     }
+
 
     const fetchDBHobbies = () => {
         fetch(`${import.meta.env.VITE_BACKEND_ADDRESS}/get-hobbies`)
@@ -45,15 +47,51 @@ function SetHobby() {
                 return userInterests.includes(hobby.interests)
             })
             const dbHobbyNames = filteredDBHobbies.map(hobby => hobby.name)
+            return dbHobbyNames
         } catch (error) {
             console.error('Error fetching hobbies:', error)
             return []
         }
     }
 
-    const fetchAPIHobbies = () => {
-        
+    useEffect(() => {
+        fetchUserInterests()
+    }, [username])
+
+  
+    const fetchAPIHobbies = async () => {   
+        try {
+            // fetchUserInterests()
+            console.log("interests", userInterests)
+            const hobbyApiKey = import.meta.env.VITE_HOBBY_API
+            const newAPIHobbies = []
+            const APIHobbies = userInterests.map(async (interest) =>
+                await fetch(`https://api.api-ninjas.com/v1/hobbies?category=${interest}`, {
+                    method: 'GET',
+                    headers: { 'X-Api-Key': hobbyApiKey }
+                })
+                .then(response => {
+                    if (response.ok) {
+                        console.log("interest", interest)
+                        return response.json()
+                    }
+                })
+                .then(data => {
+                    console.log("data", data)
+                    // setAPIHobbies([...apiHobbies, data])
+                    return data
+                })
+            )
+            return APIHobbies
+        } catch (error) {
+            console.error('error fetching api hobbies', error)
+        }
     }
+    // const check = fetchAPIHobbies()
+    // const apiHobby = check.map(response => response.data)
+    // console.log("hellooo", apiHobby)
+    console.log("test fetch", fetchAPIHobbies())
+    // console.log("check api", apiHobbies)
 
     
 
